1cc1dcaea585491c215b09b2cac9993e7378d537
Crul <rauljavier.vila@gmail.com>
2015-06-06 19:03:06 +0200

    platon report inspired refactoring
    jsRenderer includes refactored
    
    related #49

--- src/render/class/js/jsRenderer.js
+++ src/render/class/js/jsRenderer.js
@@ -1,33 +1,18 @@
-define(
-	[
-		'src/render/codeRenderer',
-		'src/render/class/js/helpers/jsUtils',
-		'src/render/class/js/helpers/jsArrays',
-		'src/render/class/js/helpers/jsCompare',
-		'src/render/class/js/helpers/jsConditional',
-		'src/render/class/js/helpers/jsConstants',
-		'src/render/class/js/helpers/jsFunctions',
-		'src/render/class/js/helpers/jsLoops',
-		'src/render/class/js/helpers/jsNotifier',
-		'src/render/class/js/helpers/jsVariables'
-	],
-	function (codeRenderer, jsUtils, jsArrays, jsCompare, jsConditional, jsConstants, jsFunctions, jsLoops, jsNotifier, jsVariables) {
-		var dis = {};
-		dis = require('util')._extend(dis, codeRenderer);
-		dis = require('util')._extend(dis, jsUtils);
+define([], function () {
+	var dis = {};
+	dis = require('util')._extend(dis, require('src/render/codeRenderer'));
+	dis = require('util')._extend(dis, require('src/render/class/js/helpers/jsUtils'));
 
-		dis.fileExtension = 'js';
+	dis.fileExtension = 'js';
 
-		dis.arrays = jsArrays;
-		dis.compare = jsCompare;
-		dis.conditional = jsConditional;
-		dis.constants = jsConstants;
-		dis.functions = jsFunctions;
-		dis.loops = jsLoops;
-		dis.notifier = jsNotifier;
-		dis.variables = jsVariables;
+	var includeHelpers = ['Arrays', 'Compare', 'Conditional', 'Constants', 'Functions', 'Loops', 'Notifier', 'Variables'];
+	
+	includeHelpers.forEach(setHelper);
+	function setHelper(helper) {
+		dis[helper.toLowerCase()] = require('src/render/class/js/helpers/js' + helper);
+	}
 
-		dis.return = jsFunctions.return; // TODO move to functions?
+	dis.return = dis.functions.return; // TODO move to functions?
 
-		return dis;
-	});
+	return dis;
+});
